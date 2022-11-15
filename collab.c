661c91f8a609392320b0ff88e3e0f94c53231484
Wajahat Ansari <mansa4@uic.edu>
2022-11-15 22:52:44 +0000

    Added Function for dkim344 and mansa4

--- collab.c
+++ collab.c
@@ -21,11 +21,20 @@ void reed_ekidan2() {
   printf("We learned that git and GitHub are amazing, powerful tools!\n");
 }
 
+void dkim_mansa3444(){
+  
+  printf("Daniel Kim and Wajahat Ansari\n");
+  
+  printf("We learned that although they sound similar, git and Github are not the same thing. Git is a way to manage repositories and versions of programs locally on your machine, while Github is a remote (cloud-based) service where you can create and store repositories. However, the two can be used in tandem to create a seamless workflow that lends itself to collaborative programming. When programming with other people, you can have a repository on Github where the team's code is to be stored. You can clone from the respository to get the code and edit it locally on your machine, and after making edits, you can push the changes/commits back to the Github repository by using git commands. This way, the whole team can have a central repository where every group member can push in their own changes to the overlying project.\n");
+  
+  printf("Two people are collaborating on this code!");
+}
+
 int main()
 {
     printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
     
-    reed_ekidan2();
+    dkim_mansa3444();
     
     return 0;
 }
